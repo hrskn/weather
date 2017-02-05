4ab28b15de86775ac1defddb92037a0d43ea2d47
jarnowatts <jarnowatts@gmail.com>
2017-02-05 16:20:30 +0200

    Weather app lat/lon

---
+++ script.js
@@ -0,0 +1,57 @@
+var api = '035a7c126e100825991fb994b6ed40d2';
+  document.body.style.backgroundColor = "#AA0000";
+function update(weather) {
+  icon.src = "http://openweathermap.org/img/w/" + weather.code + ".png"
+  temp.innerHTML = weather.temp;
+  loc.innerHTML = weather.location;
+  console.log(weather.code);
+
+
+
+
+
+}
+
+window.onload = function () {
+    temp = document.getElementById("temp");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    status = document.getElementById('status');
+
+  if(navigator.geolocation){
+	var showPosition = function(position){
+	    updateByGeo(position.coords.latitude, position.coords.longitude);
+	}
+	navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function sendRequest(url){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+	     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+         var data = JSON.parse(xmlhttp.responseText);
+	       var weather = {};
+         weather.code = data.weather[0].icon;
+	       weather.location = data.name;
+	       weather.temp = toCelcius(data.main.temp)+"°C";
+         weather.status = data.weather[0].main;
+
+	       update(weather);
+	      }
+      };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function updateByGeo(lat, lon){
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+	"lat=" + lat +
+	"&lon=" + lon +
+	"&APPID=" + api;
+  sendRequest(url);
+
+}
+function toCelcius(kelvin){
+    return Math.round(kelvin - 273.15);
+}
+}
